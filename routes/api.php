921d6103d580c1d861b725bca124b7ea5880c05c
Kunzal200 <kunzal.sohail200@gmail.com>
2026-06-26 17:53:22 +0500

    changes

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,14 @@
 use App\Http\Controllers\Api\FileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('s3-files/upload-attachment', function () {
+    return response()->json([
+        'error' => [
+            'code' => 'METHOD_NOT_ALLOWED',
+            'message' => 'This endpoint only accepts POST requests.',
+        ],
+    ], 405);
+});
+
 Route::post('s3-files/upload-attachment', [FileController::class, 'uploadAttachment']);
 Route::post('s3-files/temporary-public-url', [FileController::class, 'temporaryPublicUrl']);
